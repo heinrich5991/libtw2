d631c811032640a1208777a833110d78d8e703e5
heinrich5991 <heinrich5991@gmail.com>
2014-06-27 10:02:40 +0200

    Add write prototype

--- rust/datafile_raw.rs
+++ rust/datafile_raw.rs
@@ -195,6 +195,9 @@ impl DatafileHeaderVersion {
 			}
 		)
 	}
+	pub fn write(self, _writer: &mut Writer) -> IoResult<()> {
+		unimplemented!();
+	}
 }
 
 impl DatafileHeader {
@@ -239,9 +242,23 @@ impl DatafileHeader {
 			}
 		)
 	}
+	pub fn write(self, _writer: &mut Writer) -> IoResult<()> {
+		unimplemented!();
+	}
+}
+
+impl DatafileItemType {
+	pub fn write(self, _writer: &mut Writer) -> IoResult<()> {
+		unimplemented!();
+	}
 }
 
 impl DatafileItemHeader {
+	pub fn new(type_id: u16, id: u16, size: i32) -> DatafileItemHeader {
+		let mut result = DatafileItemHeader { type_id__id: 0, size: size };
+		result.set_type_id__id(type_id, id);
+		result
+	}
 	pub fn type_id(&self) -> u16 {
 		(((self.type_id__id as u32) >> 16) & 0xffff) as u16
 	}
@@ -769,3 +786,47 @@ impl Datafile for DatafileBuffer {
 		(item_type.start, item_type.num)
 	}
 }
+
+/*
+pub fn write_datafile<T:Datafile,W:Writer>(df: &T, writer: &mut W) -> Result<IoResult<()>,()> {
+	let compressed_data: Vec<Vec<u8>> = try!(result::collect(df.data_iter().map(|maybe_x| maybe_x.map(|x| {
+		zlib::compress_vec(x).unwrap()
+	}))));
+
+	let size_items = df.items().fold(0, |s, i| {
+		s + i.data.len() * mem::size_of::<i32>() + mem::size_of::<DatafileItemHeader>()
+	});
+
+	let size_data = compressed_data.iter().fold(0, |s, d| s + d.len());
+
+	try!(DatafileHeaderVersion {
+		magic: DATAFILE_MAGIC,
+		version: 4,
+	}.write(writer));
+
+	try!(DatafileHeader {
+		_size: unimplemented!(),
+		_swaplen: unimplemented!(),
+		num_item_types: df.item_types().len(),
+		num_items: df.items().len(),
+		num_data: df.data_iter().len(),
+		size_items: size_items,
+		size_data: size_data,
+	}.write(writer));
+
+	for &type_id in df.item_types() {
+		let (start, num) = df.item_type_indexes_start_num(type_id);
+		try!(DatafileItemType {
+			type_id: type_id.as_i32().unwrap(),
+			start: start.as_i32().unwrap(),
+			num: num.as_i32().unwrap(),
+		}.write(writer));
+	}
+
+	for DatafileItem { type_id, id, data } in df.items() {
+		try!(DatafileItemHeader::new(type_id, id, data.len()).write(writer));
+	}
+	unimplemented!();
+	Ok(Ok(()))
+}
+*/
