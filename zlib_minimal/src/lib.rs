c109687b24b6a87979d01d5afd5456c6e97f960d
heinrich5991 <heinrich5991@gmail.com>
2015-05-16 10:40:16 +0200

    zlib_minimal: Upgrade rustc

--- zlib_minimal/src/lib.rs
+++ zlib_minimal/src/lib.rs
@@ -3,9 +3,6 @@
 //! This wrapper only exposes the `uncompress` method of zlib, both without
 //! indirection and as idiomatic Rust function.
 
-#![crate_type = "rlib"]
-#![crate_type = "dylib"]
-
 extern crate libc;
 
 use libc::c_ulong;
@@ -30,10 +27,10 @@ pub mod raw {
 /// the number of bytes written. If the decompression fails for some reason,
 /// Err is returned. In this case, the `dest` buffer may or may not be
 /// modified.
-pub fn uncompress(dest: &mut [u8], src: &[u8]) -> Result<uint,()> {
+pub fn uncompress(dest: &mut [u8], src: &[u8]) -> Result<usize,()> {
     let mut output_size = dest.len() as c_ulong;
     if unsafe { raw::uncompress(dest.as_mut_ptr(), &mut output_size, src.as_ptr(), src.len() as c_ulong) } == raw::Z_OK {
-        Ok(output_size as uint)
+        Ok(output_size as usize)
     } else {
         Err(())
     }
@@ -44,10 +41,10 @@ pub fn uncompress(dest: &mut [u8], src: &[u8]) -> Result<uint,()> {
 /// Compresses the `src` parameter into the `dest` parameter and returning the
 /// number of bytes written. If the compression fails for some reason, Err is
 /// returned. In this case, the `dest` buffer may or may not be modified.
-pub fn compress(dest: &mut [u8], src: &[u8]) -> Result<uint,()> {
+pub fn compress(dest: &mut [u8], src: &[u8]) -> Result<usize,()> {
     let mut output_size = dest.len() as c_ulong;
     if unsafe { raw::compress(dest.as_mut_ptr(), &mut output_size, src.as_ptr(), src.len() as c_ulong) } == raw::Z_OK {
-        Ok(output_size as uint)
+        Ok(output_size as usize)
     } else {
         Err(())
     }
@@ -56,8 +53,8 @@ pub fn compress(dest: &mut [u8], src: &[u8]) -> Result<uint,()> {
 /// The wrapper for zlib's `compressBound` function.
 ///
 /// Returns an upper bound on the compressed size for `compress()`.
-pub fn compress_bound(source_len: uint) -> uint {
-    (unsafe { raw::compressBound(source_len as c_ulong) }) as uint
+pub fn compress_bound(source_len: usize) -> usize {
+    (unsafe { raw::compressBound(source_len as c_ulong) }) as usize
 }
 
 pub fn compress_vec(source: &[u8]) -> Result<Vec<u8>,()> {
@@ -67,7 +64,7 @@ pub fn compress_vec(source: &[u8]) -> Result<Vec<u8>,()> {
     // u8 has no destructor, this is safe
     unsafe { dest.set_len(upper_bound); }
 
-    let output_length = try!(compress(dest.as_mut_slice(), source));
+    let output_length = try!(compress(&mut dest, source));
     unsafe { dest.set_len(output_length); }
 
     Ok(dest)
