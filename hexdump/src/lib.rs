0106d070b096270c91d24a0481b3729c8c0b2626
heinrich5991 <heinrich5991@gmail.com>
2016-04-07 13:42:22 +0200

    hexdump: Remove redundant `all()` in `cfg()`

--- hexdump/src/lib.rs
+++ hexdump/src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(all(test, feature="nightly-test"), feature(plugin))]
 #![cfg_attr(all(test, feature="nightly-test"), plugin(quickcheck_macros))]
 #[cfg(all(test, feature="nightly-test"))] extern crate quickcheck;
-#[cfg(all(test))] extern crate num;
+#[cfg(test)] extern crate num;
 
 extern crate arrayvec;
 extern crate itertools;
