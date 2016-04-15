964e19d3d3ef4633e6f83a8c38b879f7b7b6c6e1
heinrich5991 <heinrich5991@gmail.com>
2016-04-15 16:42:21 +0200

    zlib_minimal: Use `libz-sys` as raw interface

--- zlib_minimal/src/lib.rs
+++ zlib_minimal/src/lib.rs
@@ -4,22 +4,9 @@
 //! indirection and as idiomatic Rust function.
 
 extern crate libc;
+extern crate libz_sys as raw;
 
 use libc::c_ulong;
-
-/// The raw interface to zlib
-pub mod raw {
-    use libc::{c_ulong, c_int};
-
-    #[link(name="z")]
-    extern {
-        pub fn uncompress(dest: *mut u8, dest_len: *mut c_ulong, source: *const u8, source_len: c_ulong) -> c_int;
-        pub fn compress(dest: *mut u8, dest_len: *mut c_ulong, source: *const u8, source_len: c_ulong) -> c_int;
-        pub fn compressBound(source_len: c_ulong) -> c_ulong;
-    }
-
-    pub static Z_OK: c_int = 0;
-}
 
 /// The wrapper for zlib's `uncompress` function.
 ///
