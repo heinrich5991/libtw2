c1cd1b6fd8cdb8627a7853129a6d6a465ab2b9d8
heinrich5991 <heinrich5991@gmail.com>
2016-05-23 09:15:08 +0200

    gamenet: Split up the `msg` module

--- gamenet/src/msg/generate_system.py
+++ gamenet/src/msg/generate_system.py
@@ -25,69 +25,14 @@ NETMSGS_SYSTEM = [
 
 header = """\
 use buffer::CapacityError;
+use bytes::PrettyBytes;
 use error::Error;
 use packer::Packer;
 use packer::Unpacker;
 use packer::with_packer;
 use std::fmt;
-
-#[derive(Clone, Copy, Debug)]
-pub struct IntegerData<'a> {
-    inner: &'a [u8],
-}
-
-impl<'a> IntegerData<'a> {
-    fn from_bytes(bytes: &[u8]) -> IntegerData {
-        IntegerData {
-            inner: bytes,
-        }
-    }
-    fn as_bytes(&self) -> &[u8] {
-        self.inner
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-enum SystemOrGame<S, G> {
-    System(S),
-    Game(G),
-}
-
-impl<S, G> SystemOrGame<S, G> {
-    fn is_game(&self) -> bool {
-        match *self {
-            SystemOrGame::System(_) => false,
-            SystemOrGame::Game(_) => true,
-        }
-    }
-    fn is_system(&self) -> bool {
-        !self.is_game()
-    }
-}
-
-impl SystemOrGame<i32, i32> {
-    fn decode_id(id: i32) -> SystemOrGame<i32, i32> {
-        let sys = id & 1 != 0;
-        let msg = id >> 1;
-        if sys {
-            SystemOrGame::System(msg)
-        } else {
-            SystemOrGame::Game(msg)
-        }
-    }
-    fn internal_id(self) -> i32 {
-        match self {
-            SystemOrGame::System(msg) => msg,
-            SystemOrGame::Game(msg) => msg,
-        }
-    }
-    fn encode_id(self) -> i32 {
-        let iid = self.internal_id() as u32;
-        assert!((iid & (1 << 31)) == 0);
-        let flag = if self.is_system() { 1 } else { 0 };
-        ((iid << 1) | flag) as i32
-    }
-}
+use super::IntegerData;
+use super::SystemOrGame;
 
 impl<'a> System<'a> {
     pub fn decode_complete(p: &mut Unpacker<'a>) -> Result<System<'a>, Error> {
@@ -196,39 +141,24 @@ def lifetime(members):
 def generate_header(msgs):
     return header
 
-def generate_mod_start(msgs):
-    return """\
-pub mod system {
-    use buffer::CapacityError;
-    use bytes::PrettyBytes;
-    use error::Error;
-    use packer::Packer;
-    use packer::Unpacker;
-    use std::fmt;
-    use super::IntegerData;
-"""
-
-def generate_mod_end(msgs):
-    return "}\n"
-
 def generate_constants(msgs):
     result = []
     for msg_id, name, _ in msgs:
-        result.append("    pub const {}: i32 = {};".format(const_name(name), msg_id))
+        result.append("pub const {}: i32 = {};".format(const_name(name), msg_id))
     result.append("")
     return "\n".join(result)
 
 def generate_structs(msgs):
     result = []
     for _, name, members in msgs:
-        result.append("    #[derive(Clone, Copy)]")
+        result.append("#[derive(Clone, Copy)]")
         if members:
-            result.append("    pub struct {}{} {{".format(struct_name(name), lifetime(members)))
+            result.append("pub struct {}{} {{".format(struct_name(name), lifetime(members)))
             for type_, opt, name in members:
-                result.append("        pub {}: {},".format(name, rust_type(type_, opt)))
-            result.append("    }")
+                result.append("    pub {}: {},".format(name, rust_type(type_, opt)))
+            result.append("}")
         else:
-            result.append("    pub struct {};".format(struct_name(name)))
+            result.append("pub struct {};".format(struct_name(name)))
         result.append("")
     return "\n".join(result)
 
@@ -237,9 +167,9 @@ def generate_struct_impl(msgs):
     for _, name, members in msgs:
         n = struct_name(name)
         l = lifetime(members)
-        result.append("    impl{l} fmt::Debug for {}{l} {{".format(n, l=l))
-        result.append("        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {")
-        result.append("            f.debug_struct(\"{}\")".format(n))
+        result.append("impl{l} fmt::Debug for {}{l} {{".format(n, l=l))
+        result.append("    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {")
+        result.append("        f.debug_struct(\"{}\")".format(n))
         for type_, opt, name in members:
             if not opt:
                 conv = ".field(\"{name}\", &{conv}(self.{name})".format
@@ -256,14 +186,14 @@ def generate_struct_impl(msgs):
                 conv = ".field(\"{name}\", &self.{name}.as_ref().map(|{name}| {pretty}({name})))"
             else:
                 raise RuntimeError("unreachable")
-            result.append(" "*4*4 + conv.format(name=name, pretty=pretty))
-        result.append("                .finish()")
-        result.append("        }")
+            result.append(" "*4*3 + conv.format(name=name, pretty=pretty))
+        result.append("            .finish()")
         result.append("    }")
-        result.append("    impl{l} {}{l} {{".format(n, l=l))
-        result.append("        pub fn decode(_p: &mut Unpacker{l}) -> Result<{}{l}, Error> {{".format(n, l=l))
+        result.append("}")
+        result.append("impl{l} {}{l} {{".format(n, l=l))
+        result.append("    pub fn decode(_p: &mut Unpacker{l}) -> Result<{}{l}, Error> {{".format(n, l=l))
         if members:
-            result.append("            Ok({} {{".format(n))
+            result.append("        Ok({} {{".format(n))
             for type_, opt, name in members:
                 if not opt:
                     conv = "try!({})".format
@@ -279,15 +209,15 @@ def generate_struct_impl(msgs):
                     decode = "_p.read_rest().map(IntegerData::from_bytes)"
                 else:
                     raise ValueError("Invalid type: {}".format(type_))
-                result.append(" "*4*4 + "{}: {},".format(name, conv(decode)))
-            result.append("            })")
+                result.append(" "*4*3 + "{}: {},".format(name, conv(decode)))
+            result.append("        })")
         else:
-            result.append("            Ok({})".format(n))
-        result.append("        }")
+            result.append("        Ok({})".format(n))
+        result.append("    }")
         if all(not opt for _, opt, _ in members):
-            result.append("        pub fn encode<'d, 's>(&self, mut _p: Packer<'d, 's>)")
-            result.append("            -> Result<&'d [u8], CapacityError>".format(n, l=l))
-            result.append("        {")
+            result.append("    pub fn encode<'d, 's>(&self, mut _p: Packer<'d, 's>)")
+            result.append("        -> Result<&'d [u8], CapacityError>".format(n, l=l))
+            result.append("    {")
             if members:
                 for type_, _, name in members:
                     if type_ == 'string':
@@ -300,10 +230,10 @@ def generate_struct_impl(msgs):
                         encode = "_p.write_rest({}.as_bytes())".format
                     else:
                         raise ValueError("Invalid type: {}".format(type_))
-                    result.append(" "*4*3 + "try!({});".format(encode("self.{}".format(name))))
-            result.append("            Ok(_p.written())")
-            result.append("        }")
-        result.append("    }")
+                    result.append("        try!({});".format(encode("self.{}".format(name))))
+            result.append("        Ok(_p.written())")
+            result.append("    }")
+        result.append("}")
         result.append("")
     return "\n".join(result)
 
@@ -315,7 +245,7 @@ def generate_enum(msgs):
     result.append("#[derive(Clone, Copy)]")
     result.append("pub enum System<'a> {")
     for _, name, members in msgs:
-        result.append("    {s}(system::{s}{}),".format(lifetime(members), s=struct_name(name)))
+        result.append("    {s}({s}{}),".format(lifetime(members), s=struct_name(name)))
     result.append("}")
     result.append("")
     return "\n".join(result)
@@ -324,7 +254,6 @@ def generate_enum_impl(msgs):
     result = []
     result.append("impl<'a> System<'a> {")
     result.append("    pub fn decode(msg_id: i32, p: &mut Unpacker<'a>) -> Result<System<'a>, Error> {")
-    result.append("        use self::system::*;")
     result.append("        Ok(match msg_id {")
     for _, name, _ in msgs:
         result.append("            {} => System::{n}(try!({n}::decode(p))),".format(const_name(name), n=struct_name(name)))
@@ -332,7 +261,6 @@ def generate_enum_impl(msgs):
     result.append("        })")
     result.append("    }")
     result.append("    pub fn msg_id(&self) -> i32 {")
-    result.append("        use self::system::*;")
     result.append("        match *self {")
     for _, name, members in msgs:
         result.append("            System::{}(_) => {},".format(struct_name(name), const_name(name)))
@@ -360,12 +288,10 @@ def main():
     msgs = make_msgs(NETMSGS_SYSTEM)
     steps = [
         generate_header,
-        generate_mod_start,
         generate_constants,
         generate_structs,
         generate_struct_impl,
         generate_system_extra,
-        generate_mod_end,
         generate_enum,
         generate_enum_impl,
     ]
---
+++ gamenet/src/msg/mod.rs
@@ -0,0 +1,62 @@
+pub use self::system::System;
+
+pub mod system;
+pub mod game;
+
+#[derive(Clone, Copy, Debug)]
+pub struct IntegerData<'a> {
+    inner: &'a [u8],
+}
+
+impl<'a> IntegerData<'a> {
+    fn from_bytes(bytes: &[u8]) -> IntegerData {
+        IntegerData {
+            inner: bytes,
+        }
+    }
+    fn as_bytes(&self) -> &[u8] {
+        self.inner
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum SystemOrGame<S, G> {
+    System(S),
+    Game(G),
+}
+
+impl<S, G> SystemOrGame<S, G> {
+    fn is_game(&self) -> bool {
+        match *self {
+            SystemOrGame::System(_) => false,
+            SystemOrGame::Game(_) => true,
+        }
+    }
+    fn is_system(&self) -> bool {
+        !self.is_game()
+    }
+}
+
+impl SystemOrGame<i32, i32> {
+    fn decode_id(id: i32) -> SystemOrGame<i32, i32> {
+        let sys = id & 1 != 0;
+        let msg = id >> 1;
+        if sys {
+            SystemOrGame::System(msg)
+        } else {
+            SystemOrGame::Game(msg)
+        }
+    }
+    fn internal_id(self) -> i32 {
+        match self {
+            SystemOrGame::System(msg) => msg,
+            SystemOrGame::Game(msg) => msg,
+        }
+    }
+    fn encode_id(self) -> i32 {
+        let iid = self.internal_id() as u32;
+        assert!((iid & (1 << 31)) == 0);
+        let flag = if self.is_system() { 1 } else { 0 };
+        ((iid << 1) | flag) as i32
+    }
+}
