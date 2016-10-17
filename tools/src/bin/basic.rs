c03ea80b429ae03eaa8afae2a13e3b00d73c04be
heinrich5991 <heinrich5991@gmail.com>
2016-10-17 15:21:36 +0200

    tools: Add a basic map checker and move more thorough one

---
+++ tools/src/bin/basic.rs
@@ -0,0 +1,20 @@
+#![cfg(not(test))]
+
+extern crate datafile as df;
+extern crate map;
+extern crate tools;
+
+use std::path::Path;
+
+fn process(_: &Path, _: df::Reader, _: &mut ())
+    -> Result<(), map::Error>
+{
+    Ok(())
+}
+
+fn print_stats(_: &()) {
+}
+
+fn main() {
+    tools::map_stats::stats(process, print_stats);
+}
---
+++ tools/src/bin/datafile.rs
@@ -0,0 +1,21 @@
+#![cfg(not(test))]
+
+extern crate datafile as df;
+extern crate map;
+extern crate tools;
+
+use std::path::Path;
+
+fn process(_: &Path, mut df: df::Reader, _: &mut ())
+    -> Result<(), map::Error>
+{
+    try!(df.debug_dump());
+    Ok(())
+}
+
+fn print_stats(_: &()) {
+}
+
+fn main() {
+    tools::map_stats::stats(process, print_stats);
+}
