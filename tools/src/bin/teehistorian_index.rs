9142d01d5f4e176a0cd4710e8a9633b309a202a3
heinrich5991 <heinrich5991@gmail.com>
2018-01-18 14:59:48 +0100

    tools: Allow `teehistorian_index` to build upon a previous index

--- tools/src/bin/teehistorian_index.rs
+++ tools/src/bin/teehistorian_index.rs
@@ -1,5 +1,6 @@
 extern crate clap;
 extern crate csv;
+extern crate itertools;
 extern crate logger;
 extern crate serde;
 #[macro_use] extern crate serde_derive;
@@ -7,11 +8,18 @@ extern crate teehistorian;
 extern crate uuid;
 extern crate walkdir;
 
+use itertools::Itertools;
+use itertools::sorted;
 use std::borrow::Cow;
 use std::ffi::OsStr;
+use std::fmt;
+use std::fs::File;
+use std::io::Write;
 use std::io;
 use std::path::Path;
+use std::path::PathBuf;
 use std::process;
+use std::slice;
 use teehistorian::Buffer;
 use teehistorian::Reader;
 use teehistorian::format;
@@ -48,7 +56,7 @@ impl From<walkdir::Error> for Error {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 struct HexU32(u32);
 
 impl serde::Serialize for HexU32 {
@@ -56,6 +64,34 @@ impl serde::Serialize for HexU32 {
         where S: serde::Serializer,
     {
         serializer.serialize_str(&format!("{:08x}", self.0))
+    }
+}
+
+struct HexU32Visitor;
+
+impl<'de> serde::de::Visitor<'de> for HexU32Visitor {
+    type Value = HexU32;
+
+    fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("8 character hex value")
+    }
+    fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<HexU32, E> {
+        let len = v.chars().count();
+        if len != 8 {
+            return Err(E::invalid_length(len, &self));
+        }
+        let value = u32::from_str_radix(v, 16).map_err(|_| {
+            E::invalid_value(serde::de::Unexpected::Str(v), &self)
+        })?;
+        Ok(HexU32(value))
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for HexU32 {
+    fn deserialize<D>(deserializer: D) -> Result<HexU32, D::Error>
+        where D: serde::de::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(HexU32Visitor)
     }
 }
 
@@ -75,11 +111,49 @@ struct Record<'a> {
     map_size: u32,
 }
 
-fn handle_dir(dir: &OsStr) -> Result<(), ()> {
-    fn helper(dir: &OsStr) -> Result<(), Error> {
+impl<'a> From<&'a ReadRecord> for Record<'a> {
+    fn from(r: &'a ReadRecord) -> Record<'a> {
+        Record {
+            path: &r.path,
+            game_uuid: r.game_uuid,
+            timestamp: Cow::from(&r.timestamp[..]),
+            map_name: Cow::from(&r.map_name[..]),
+            map_crc: r.map_crc.into(),
+            map_size: r.map_size,
+        }
+    }
+}
+
+fn contains<'a>(
+    base: &mut slice::Iter<'a, ReadRecord>,
+    writer: &mut csv::Writer<Box<Write>>,
+    path: &Path,
+) -> Result<bool, Error>
+{
+    let mut found = false;
+    for record in base.peeking_take_while(|r| r.path <= path) {
+        if record.path == path {
+            found = true;
+        }
+        writer.serialize(Record::from(record))?;
+    }
+    Ok(found)
+}
+
+fn handle_dir<'a>(
+    base: &mut slice::Iter<'a, ReadRecord>,
+    writer: &mut csv::Writer<Box<Write>>,
+    dir: &Path,
+) -> Result<(), ()>
+{
+    fn helper<'a>(
+        base: &mut slice::Iter<'a, ReadRecord>,
+        writer: &mut csv::Writer<Box<Write>>,
+        dir: &Path,
+    ) -> Result<(), Error>
+    {
         let mut buffer = Buffer::new();
-        let mut writer = csv::Writer::from_writer(io::stdout());
-        for entry in WalkDir::new(Path::new(dir))
+        for entry in WalkDir::new(dir)
             .sort_by(|a, b| a.file_name().cmp(b.file_name()))
         {
             let entry = entry?;
@@ -87,6 +161,9 @@ fn handle_dir(dir: &OsStr) -> Result<(), ()> {
                 continue;
             }
             if entry.path().extension() != Some(OsStr::new("teehistorian")) {
+                continue;
+            }
+            if contains(base, writer, entry.path())? {
                 continue;
             }
             buffer.clear();
@@ -108,17 +185,67 @@ fn handle_dir(dir: &OsStr) -> Result<(), ()> {
         }
         Ok(())
     }
-    helper(dir).map_err(|e| eprintln!("{}: {:?}", Path::new(dir).display(), e))
+    helper(base, writer, dir).map_err(|e| eprintln!("{}: {:?}", dir.display(), e))
 }
 
-fn handle_args(args: Option<clap::OsValues>) -> Result<(), ()> {
-    if let Some(paths) = args {
-        for path in paths {
-            handle_dir(path)?;
-        }
-    } else {
-        handle_dir(OsStr::new("."))?;
+// Why do I need a separate one for this. :(
+//
+// `Ord` is implemented using mainly `path`.
+#[derive(Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd)]
+struct ReadRecord {
+    path: PathBuf,
+    game_uuid: Uuid,
+    timestamp: String,
+    map_name: String,
+    map_crc: HexU32,
+    map_size: u32,
+}
+
+fn read_index(path: &Path) -> Result<Vec<ReadRecord>, Error> {
+    fn read(path: &Path) -> Result<Vec<ReadRecord>, Error> {
+        csv::Reader::from_path(path)?
+            .into_deserialize()
+            .map(|r| r.map_err(|e| e.into()))
+            .collect()
     }
+    read(path).map(|mut v| { v.sort(); v })
+}
+
+fn swap<T, E>(x: Option<Result<T, E>>) -> Result<Option<T>, E> {
+    match x {
+        Some(Ok(x)) => Ok(Some(x)),
+        Some(Err(x)) => Err(x),
+        None => Ok(None)
+    }
+}
+
+fn handle_args(
+    base: Option<&Path>,
+    output: Option<&Path>,
+    dirs: Vec<PathBuf>,
+) -> Result<(), ()>
+{
+    let base = swap(base.map(|b| {
+        read_index(b).map_err(|e| eprintln!("{}: {:?}", b.display(), e))
+    }))?.unwrap_or(Vec::new());
+
+    let mut base_iter = base.iter();
+
+    let mut csv_out: csv::Writer<Box<Write>> = csv::Writer::from_writer(match output {
+        Some(o) => Box::new(File::create(o).map_err(|e| {
+            eprintln!("{}: {:?}", o.display(), e)
+        })?),
+        None => Box::new(io::stdout()),
+    });
+
+    for dir in dirs {
+        handle_dir(&mut base_iter, &mut csv_out, &dir)?;
+    }
+
+    for record in base_iter {
+        csv_out.serialize(Record::from(record)).map_err(|e| eprintln!("{:?}", e))?;
+    }
+
     Ok(())
 }
 
@@ -129,8 +256,21 @@ fn main() {
     logger::init();
 
     let matches = App::new("Teehistorian indexer")
-        .about("Indexes folders of teehistorian files and dumps the index into\
+        .about("Indexes folders of teehistorian files and dumps the index into \
                 a CSV file")
+        .arg(Arg::with_name("base")
+            .short("b")
+            .long("base")
+            .value_name("BASE")
+            .help("Sets a base index file")
+        )
+        .arg(Arg::with_name("inplace")
+            .short("i")
+            .long("in-place")
+            .value_name("INDEX")
+            .help("Sets the index file to update")
+            .conflicts_with("base")
+        )
         .arg(Arg::with_name("DIRECTORY")
             .help("Directories to scan (current directory if none are given)")
             .multiple(true)
@@ -138,8 +278,22 @@ fn main() {
         .get_matches();
 
     let paths = matches.values_of_os("DIRECTORY");
+    let base = matches.value_of_os("base");
+    let inplace = matches.value_of_os("inplace");
+    let (input, output) = match (base, inplace) {
+        (None, None) => (None, None),
+        (Some(b), None) => (Some(Path::new(b)), None),
+        (None, Some(i)) => (Some(Path::new(i)), Some(Path::new(i))),
+        (Some(_), Some(_)) => unreachable!(),
+    };
 
-    if handle_args(paths).is_err() {
+    let dirs = if let Some(p) = paths {
+        sorted(p.into_iter().map(|p| PathBuf::from(p)))
+    } else {
+        vec![PathBuf::from(".")]
+    };
+
+    if handle_args(input, output, dirs).is_err() {
         process::exit(1);
     }
 }
