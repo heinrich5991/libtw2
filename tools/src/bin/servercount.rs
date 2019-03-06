d02b59d8612c8d1ad5a34a9e232ccec480cdd4d8
heinrich5991 <heinrich5991@gmail.com>
2019-03-06 09:55:54 +0100

    tools: Add servercount tool that was lying around

---
+++ tools/src/bin/servercount.rs
@@ -0,0 +1,43 @@
+#![cfg(not(test))]
+
+#[macro_use]
+extern crate log;
+extern crate serverbrowse;
+extern crate tools;
+
+use serverbrowse::protocol as browse_protocol;
+use serverbrowse::protocol::CountResponse;
+use serverbrowse::protocol::Response;
+
+use std::net::SocketAddr;
+use std::net::UdpSocket;
+
+const BUFSIZE: usize = 2048;
+
+fn do_(socket: UdpSocket, addr: SocketAddr) {
+    let mut buf = [0; BUFSIZE];
+
+    socket.send_to(&browse_protocol::request_count(), addr).unwrap();
+
+    loop {
+        let (len, from) = socket.recv_from(&mut buf).unwrap();
+        if from != addr {
+            error!("received response from non-peer, wanted={} got={}", addr, from);
+            continue;
+        }
+
+        match browse_protocol::parse_response(&buf[..len]) {
+            Some(Response::Count(CountResponse(x))) => {
+	    	println!("{}", x);
+                break;
+            },
+            _ => {
+                error!("received non-info response from peer");
+            },
+        }
+    }
+}
+
+fn main() {
+    tools::client::client(do_);
+}
