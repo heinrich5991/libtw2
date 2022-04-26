9fbebeb34c7f56741307cd1685dbcfcc5b2a5929
heinrich5991 <heinrich5991@gmail.com>
2022-04-26 17:19:42 +0200

    wireshark-dissector: Update to Wireshark 3.6
    I already had this lying around since early February, apparently. :o
    
    Fixes #52.

--- wireshark-dissector/src/lib.rs
+++ wireshark-dissector/src/lib.rs
@@ -79,7 +79,7 @@ pub static plugin_want_major: c_int = 3;
 
 #[allow(non_upper_case_globals)]
 #[no_mangle]
-pub static plugin_want_minor: c_int = 4;
+pub static plugin_want_minor: c_int = 6;
 
 #[allow(non_upper_case_globals)]
 #[no_mangle]
