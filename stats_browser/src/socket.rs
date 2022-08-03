460352efb1e043520ef3395f4077edb1cd4ec970
heinrich5991 <heinrich5991@gmail.com>
2022-08-04 01:09:03 +0200

    stats_browser: Move dependency from mio 0.5 to mio 0.6
    This allows upgrading miow 0.2.1 to 0.2.2 (due to CVE-2020-35921).

--- stats_browser/src/socket.rs
+++ stats_browser/src/socket.rs
@@ -1,6 +1,6 @@
 extern crate mio;
 
-use self::mio::udp::UdpSocket as MioUdpSocket;
+use self::mio::net::UdpSocket as MioUdpSocket;
 
 use std::fmt;
 use std::io;
@@ -10,17 +10,25 @@ use addr::Addr;
 /// An unconnected non-blocking UDP socket.
 pub struct UdpSocket(MioUdpSocket);
 
+fn non_block<T>(res: io::Result<T>) -> io::Result<Option<T>> {
+    match res {
+        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => Ok(None),
+        Err(e) => Err(e),
+        Ok(x) => Ok(Some(x)),
+    }
+}
+
 impl UdpSocket {
     /// Opens a UDP socket.
     pub fn open() -> SockResult<UdpSocket> {
-        MioUdpSocket::v6()
+        MioUdpSocket::bind(&"[::]:0".parse().unwrap())
             .map(|s| UdpSocket(s))
             .map_err(|e| SockError(e))
     }
     /// Sends a UDP packet to the specified address. Non-blocking.
     pub fn send_to(&mut self, buf: &[u8], dst: Addr) -> SockResult<NonBlock<()>> {
         let &mut UdpSocket(ref mut std_sock) = self;
-        match std_sock.send_to(buf, &dst.to_socket_addr()) {
+        match non_block(std_sock.send_to(buf, &dst.to_socket_addr())) {
             Ok(Some(len)) => {
                 assert!(len == buf.len(), "short send: {} out of {}", len, buf.len());
                 Ok(Ok(()))
@@ -34,7 +42,7 @@ impl UdpSocket {
     /// Returns number number of bytes read and the source address.
     pub fn recv_from(&mut self, buf: &mut [u8]) -> SockResult<NonBlock<(usize, Addr)>> {
         let &mut UdpSocket(ref mut std_sock) = self;
-        match std_sock.recv_from(buf) {
+        match non_block(std_sock.recv_from(buf)) {
             Ok(Some((len, sockaddr))) => {
                 let from = Addr::from_socket_addr(sockaddr);
                 Ok(Ok((len, from)))
