b290d14c107819a24add30b78564ed20701cd19c
heinrich5991 <heinrich5991@gmail.com>
2022-09-22 02:11:35 +0200

    wireshark-dissector: Be more precise where to get dynamic symbols
    The symbol `wmem_alloc` moved from libwireshark to libwsutil, but only
    Windows users were affected because only Windows cared where the symbols
    come from.
    
    Fixes #61.

--- wireshark-dissector/build.rs
+++ wireshark-dissector/build.rs
@@ -16,20 +16,25 @@ fn windows_msvc() {
         .into_string().expect("invalid TARGET");
 
     let lib = cc::windows_registry::find_tool(&target, "lib.exe").expect("lib.exe not found");
-    let def_file = top_level.join("sys\\src\\libwireshark.def");
-    let def_file = def_file.into_os_string().into_string().expect("invalid CARGO_MANIFEST_DIR");
+    let build_and_link_lib = |def_filename: &str, lib_filename: &str| {
+        let def_file = top_level.join(def_filename);
+        let def_file = def_file.into_os_string().into_string().expect("invalid CARGO_MANIFEST_DIR");
 
-    let lib_file = out_dir.join("libwireshark.lib");
-    let lib_file = lib_file.into_os_string().into_string().expect("invalid OUT_DIR");
+        let lib_file = out_dir.join(lib_filename);
+        let lib_file = lib_file.into_os_string().into_string().expect("invalid OUT_DIR");
 
-    let result = lib.to_command()
-        .arg(format!("/def:{}", def_file))
-        .arg(format!("/out:{}", lib_file))
-        .status()
-        .expect("failed to execute lib.exe");
-    assert!(result.success());
+        let result = lib.to_command()
+            .arg(format!("/def:{}", def_file))
+            .arg(format!("/out:{}", lib_file))
+            .status()
+            .expect("failed to execute lib.exe");
+        assert!(result.success());
 
-    println!("cargo:rustc-cdylib-link-arg={}", lib_file);
+        println!("cargo:rustc-cdylib-link-arg={}", lib_file);
+    };
+
+    build_and_link_lib("sys\\src\\libwireshark.def", "libwireshark.lib");
+    build_and_link_lib("sys\\src\\libwsutil.def", "libwsutil.lib");
 }
 
 fn main() {
--- wireshark-dissector/sys/extract_def_file.py
+++ wireshark-dissector/sys/extract_def_file.py
@@ -1,7 +1,10 @@
+import os.path
 import re
+import subprocess
+import sys
 
 RE_FUNCTION_NAME=re.compile(r'^    pub fn (?P<name>[A-Za-z0-9_]+).*$')
-def extract_exported_functions(lines):
+def extract_imported_functions(lines):
     result = []
     in_extern_c = False
     for line in lines:
@@ -14,21 +17,54 @@ def extract_exported_functions(lines):
             result.append(m.group("name"))
     return sorted(result)
 
-def do(filename, output_def):
+def extract_exported_functions(library):
+    """
+    Returns the list of all exported functions from the shared object specified
+    by the file path `library`.
+    """
+    output = subprocess.check_output(["nm", "-D", library], encoding="utf8")
+    symbols = [line.split() for line in output.splitlines()]
+    return [name for (*_, type_, name) in symbols if type_ == "T"]
+
+def write_def_file(filename, symbols):
+    result = ["EXPORTS\n"] + ["    {}\n".format(name) for name in symbols]
+    with open(filename, "w") as f:
+        f.write("".join(result))
+
+def do(filename, libs_path, def_path, libraries):
     with open(filename) as f:
         input = list(f)
-    function_names = extract_exported_functions(input)
-    result = ["EXPORTS\n"] + ["    {}\n".format(name) for name in function_names]
-    with open(output_def, "w") as f:
-        f.write("".join(result))
+    wanted_function_names = extract_imported_functions(input)
+    have_function_names = {}
+    for library in libraries:
+        library_path = os.path.join(libs_path, "lib{}.so".format(library))
+        for symbol in extract_exported_functions(library_path):
+            if symbol in have_function_names:
+                print("Found symbol {} in two different libraries: {} and {}".format(symbol, have_function_names[symbol], library), file=sys.stderr)
+                return 1
+            have_function_names[symbol] = library
+
+    missing_function_names = set(wanted_function_names) - set(have_function_names)
+    if missing_function_names:
+        print("Can't find the following symbol(s) in any of the given libraries: {}".format(", ".join(sorted(missing_function_names))))
+        return 1
+
+    imports = {library: [] for library in libraries}
+    for symbol in wanted_function_names:
+        imports[have_function_names[symbol]].append(symbol)
+
+    for library in libraries:
+        write_def_file(os.path.join(def_path, "lib{}.def".format(library)), sorted(imports[library]))
 
 def main():
     import argparse
-    p = argparse.ArgumentParser(description="Generate a .def file from a bindgen-generated Wireshark binding")
+    p = argparse.ArgumentParser(description="Generates .def files from a bindgen-generated Wireshark binding and Wireshark libraries. Only works on Linux and requires the `nm` tool.")
     p.add_argument("filename", metavar="FILENAME", help="bindgen-generated Wireshark binding .rs file")
-    p.add_argument("output_def", metavar="OUTPUT_DEF", help="Path to .def file to produce")
+    p.add_argument("libs_path", metavar="LIBS_PATH", help="Path to the libraries to take the symbols from")
+    p.add_argument("def_path", metavar="DEF_PATH", help="Output path for the .def files")
+    p.add_argument("libraries", metavar="LIBRARY", nargs="+", help="Library names (without lib prefix and without .so suffix)")
     args = p.parse_args()
-    do(args.filename, args.output_def)
+    sys.exit(do(args.filename, args.libs_path, args.def_path, args.libraries))
 
 if __name__ == '__main__':
     main()
