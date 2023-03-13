22f533cab5ec5bc0599e491d502f978bdbcee3f6
heinrich5991 <heinrich5991@gmail.com>
2023-03-14 00:35:36 +0100

    tools/serverlist7: Wait for packets indefinitely

--- tools/src/bin/serverlist7.rs
+++ tools/src/bin/serverlist7.rs
@@ -48,7 +48,6 @@ fn do_(socket: UdpSocket, addr: SocketAddr) {
                 for &s in list.iter() {
                     println!("{}", s.unpack());
                 }
-                break;
             },
             _ => {
                 error!("received non-list response from peer");
